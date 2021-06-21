a57ec4e56703e65668c67d2349fe7168ff025e25
razcohen <raz@icohens.com>
2021-06-21 19:08:02 +0300

    initial commit

---
+++ web/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './posts/post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ web/src/app/posts/post/post.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css'],
+})
+export class PostComponent implements OnInit {
+  post: Post = {
+    id: 1,
+    name: 'New Post',
+    content: 'just some random description',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
